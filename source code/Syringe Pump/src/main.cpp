8464696a2e652c6ab47214c8deb6317db106f636
ChandanaWhizdata <chandanar@entrix.in>
2023-04-10 12:30:04 +0530

    main source file update

--- source code/Syringe Pump/src/main.cpp
+++ source code/Syringe Pump/src/main.cpp
@@ -1,4 +1,24 @@
-#include <Arduino.h>
+/*******************************************************************************
+  Syringe Pump Control Source file 
+
+  Company:
+    Whizdata Solutions LLP.
+
+  File Name:
+    main.cpp
+
+  Summary:
+    This file contains all the functions related to syringe pump control
+
+  Description:
+    This file contains implementation of the main control functions
+ 
+ *******************************************************************************/
+
+/*******************************************************************************
+Header Inclusions
+*******************************************************************************/
+#include <main.h>
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,3 +27,5 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+void System_Ii
