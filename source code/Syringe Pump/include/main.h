d3a46d0a3d64c17a5b74358cf970bf9a6b13cb37
ChandanaWhizdata <chandanar@entrix.in>
2023-04-10 12:09:51 +0530

    empty main.h file

---
+++ source code/Syringe Pump/include/main.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+  System Definitions
+
+  File Name:
+    main.h
+
+  Summary:
+    Header file which shares global variables and function prototypes.
+
+  Description:
+    This file contains the global variables and function prototypes for a syringe pump control project.
+
+ *******************************************************************************/
+
+// ensure this library description is only included once
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#endif  //MAIN_H
